b31b2a224751ea0db9094d5cdf9031b932d0d3e9
mattiapocci <mattiapocci@hotmail.it>
2021-10-28 18:18:38 +0200

    build_dataset

---
+++ SAFETorch/build_dataset.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import torch
+from tqdm import tqdm
+
+folder = sys.argv[1]
+
+#pick all exes in folder
+ls = list(filter(lambda elem: 'exe' in elem, os.listdir(folder)))
+
+exploits_embeddings = {}
+
+for exe in tqdm(ls):
+    print('Beginning ' + exe)
+    exploits_embeddings[exe.replace('.exe','')] = {}
+    os.system('python embeddings_extractor.py ' + folder + exe)
+    exploits_embeddings[exe.replace('.exe','')] = torch.load('/root/poc_detection/SAFETorch/SAFEtorch/input_exe_embeddings.pt')
+    os.remove('/root/poc_detection/SAFETorch/SAFEtorch/input_exe_embeddings.pt')
+    print('Finished ' + exe)
+
+# remove invalid entries in exploits_embeddings
+from copy import deepcopy
+d = deepcopy(exploits_embeddings)
+# Check if a tensor is zero: if my_tensor.float().sum().data[0] == 0:
+for key in d:
+    for minikey in d[key]:
+        if d[key][minikey].float().sum().item() == 0:
+            del exploits_embeddings[key][minikey]
+
+torch.save(exploits_embeddings,'/root/poc_detection/datasets/exploits_embeddings.pt')
