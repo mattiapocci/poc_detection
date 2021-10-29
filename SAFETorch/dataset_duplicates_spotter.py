21c17ecdf648bf0b2c9b101a50f5551f3007be3a
mattiapocci <mattiapocci@hotmail.it>
2021-10-29 18:31:27 +0200

    duplicates spotter now removes duplicates

--- SAFETorch/dataset_duplicates_spotter.py
+++ SAFETorch/dataset_duplicates_spotter.py
@@ -1,8 +1,11 @@
 from itertools import chain
 import torch
+import os
+#da levare se docker container
+os.chdir('SAFETorch')
 
 dataset = torch.load('../datasets/exploits_embeddings_complete.pt')
-
+torch.save(dataset,'../datasets/exploits_embeddings_complete_with_duplicates.pt')
 rev_dict = {}
 for key, value in dataset.items():
 	rev_dict.setdefault(str(value), set()).add(key)
@@ -11,6 +14,11 @@ for key, value in dataset.items():
 result = filter(lambda x: len(x)>1, rev_dict.values())
 res = list(result)
 for s in res:
-    print(s)
-    print(len(s))
-    print('---')
+    primo = True
+    for minis in s:
+        if not primo:
+            del dataset[minis]
+        else:
+            primo = False
+
+torch.save(dataset,'../datasets/exploits_embeddings_complete.pt')
