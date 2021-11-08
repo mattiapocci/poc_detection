f88029534c44e6334070f4903cd30ed47c27f9c4
mattiapocci <mattiapocci@hotmail.it>
2021-11-08 20:19:51 +0100

    dicts_similarity_analyzer

---
+++ SAFETorch/dicts_similarity_analyzer.py
@@ -0,0 +1,25 @@
+import torch
+import os
+import sys
+from tqdm import tqdm
+try:
+    means_path = sys.argv[1]
+except:
+    print("Usage: python dicts_similarity_analyzer.py <path_to_means_file> <optional_threshold>")
+    os.exit(-1)
+
+try:
+    threshold = float(sys.argv[2])
+except:
+    threshold = 0.8
+
+means = torch.load(means_path)
+
+for exe in tqdm(means):
+    
+    values = means[exe]
+    for key in values:
+        if values[key] > threshold:
+            print(exe + ' has similarity ' + str(values[key]) + ' with poc ' + str(key) + '.')
+    
+
