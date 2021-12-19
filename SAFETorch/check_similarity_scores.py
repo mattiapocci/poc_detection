98c83d44b339e3c808afb64435c90cd4c2c06438
mattiapocci <mattiapocci@hotmail.it>
2021-12-19 12:03:07 +0100

    check similarity scores

--- SAFETorch/check_similarity_scores.py
+++ SAFETorch/check_similarity_scores.py
@@ -6,12 +6,17 @@ try:
     n = int(sys.argv[2])
 except:
     n = 10
-scores = torch.load(scores_path)
-scores = dict(sorted(scores.items(), key=lambda item: item[1], reverse=True))
-i = 1
-print('Top ' + str(n) + ' similar exploits: ')
-for elem in scores:
-    if i>n:
-        sys.exit(0)
-    print('('+ str(i) + ') '+ 'ID: ' + str(elem) + ' Similarity: ' + str(scores[elem]))
-    i = i + 1
+input = torch.load(scores_path)
+
+
+
+for score in input:
+    scores = input[score]
+    scores = dict(sorted(scores.items(), key=lambda item: item[1], reverse=True))
+    i = 1
+    print('Top ' + str(n) + ' similar exploits: ')
+    for elem in scores:
+        if i>n:
+            sys.exit(0)
+        print('('+ str(i) + ') '+ 'ID: ' + str(elem) + ' Similarity: ' + str(scores[elem]))
+        i = i + 1
